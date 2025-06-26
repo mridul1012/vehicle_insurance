fe18b22b6aac51202c1e16d7e645f4f8e1c4ff4c
Mridul Agrawal <mridul16011996@gmail.com>
2025-06-26 21:24:59 +0530

    commit 1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mridul Agrawal",
+    author_email="mridul16011996@gmail.com",
+    packages=find_packages()
+)
