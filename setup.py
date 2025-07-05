9f103b133a37be9dac2f8f1b656c2697e2fdd424
Mridul Agrawal <mridul16011996@gmail.com>
2025-07-05 13:06:00 +0530

    till logging and exception

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="src",
-    version="0.0.1",
-    author="Mridul Agrawal",
-    author_email="mridul16011996@gmail.com",
-    packages=find_packages()
-)
---
+++ src/configuration/aws_connection.py
@@ -0,0 +1,3 @@
+from components.data_ingestion import add
+
+print(add(3))
